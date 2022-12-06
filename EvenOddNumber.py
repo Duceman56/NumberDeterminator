02117ad646788a7375f094a8cde0b7e960e603b2
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-12-05 21:20:02 -0600

    Update EvenOddNumber.py

--- EvenOddNumber.py
+++ EvenOddNumber.py
@@ -1,18 +1,12 @@
 evenFactor = 2
 
-
-
-
 #PARAMETER: inputValue is float type
-#PARAMETER: outputType is string type
 
 def evenOddNumber (inputValue) : 
-    if (inputValue / evenFactor)  :
+    if (inputValue / evenFactor).is_integer() :
         return("Even")
     else :
-        print("Odd")
-
-
+        return("Odd")
 
 #driver
-print(evenOddNumber(11))
+print(evenOddNumber(22))
