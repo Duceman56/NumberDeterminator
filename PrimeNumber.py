e320b9856417fbd3614be9f52c85e67d1ea18787
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-12-06 14:00:42 -0600

    Update PrimeNumber.py

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,9 +1,6 @@
-primeNumberFactor = 2
-
 #PARAMETER: inputValue is float type
 
 def primeNumber (inputValue) : 
-    itself = inputValue
     if (inputValue / iter(1)).is_integer() :
         return("Good Boi")
     else :
