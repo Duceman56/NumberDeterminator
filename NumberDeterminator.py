dae40b5177f0fc83d40b5f4794594c8a6f4e05ce
Graham Waters <graham.waters.business@gmail.com>
2022-12-07 12:09:27 -0600

    main: Updated

--- NumberDeterminator.py
+++ NumberDeterminator.py
@@ -2,17 +2,25 @@ evenFactor = 2
 
 #PARAMETER: inputValue is float type
 
-def determineNumber(inputValue) : 
-    if (inputValue / evenFactor).is_integer() :
+def determineNumber(inputValue) :
+    """
+    determineNumber determines if the input value is even or odd
+
+    _extended_summary_
+
+    :param inputValue: the value to be determined
+    :type inputValue: float
+    """
+    if (inputValue / evenFactor).is_integer() : # if the input value is even
         return("Even")
     else :
         return("Odd")
-        
-    if inputValue / 1 == 0 :
+
+    if inputValue / 1 == 0 : # if the input value is a whole number
         return("True")
     else :
         return("False")
-    
+
 
 
 #driver
