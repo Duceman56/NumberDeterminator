48555e219d6d0e33cd0b00840b10844e3db33f28
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-12-08 16:33:46 -0600

    Update PrimeNumber.py

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,8 +1,10 @@
 #PARAMETER: inputValue is float type
 
-def primeNumber (inputValue) : 
-    MUST USE MODULUS (%)
+#def primeNumber (inputValue) : 
+ #   MUST USE MODULUS (%)
     
 
 #driver
-print(primeNumber(11))
+#print(primeNumber(11))
+
+print(4%2)
